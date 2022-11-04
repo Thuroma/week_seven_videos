df83989d5f88069098a2fccc779bcaf2f7b8e639
Thuroma <thurman.thoma@gmail.com>
2022-11-04 12:38:11 -0500

    Finishing the videos

--- iss_webpage/fetch.js
+++ iss_webpage/fetch.js
@@ -7,6 +7,7 @@ let issLong = document.querySelector('#iss-long')
 let displayUpdateTime = document.querySelector('#iss-update-time')
 
 let update = 10000
+let maxFailedAttempts = 3
 let issMarker
 let icon = L.icon({
     iconUrl: 'iss_icon.png',
@@ -20,36 +21,46 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-iss()   // call function one time to start
-setInterval(iss, update)     // 10 seconds
+iss(maxFailedAttempts)   // call function one time to start
+// setInterval(iss, update)     // 10 seconds - changed to recursive setTimeout
 
 
 // set up the iss function from the callback setInterval function
 // this gets updated every 10 seconds
-function iss() {
-// fetch the api data
-    fetch(url).then(res => { // res = response
-        return res.json()
-    }).then( (issData) => {
-        console.log(issData)
-        let lat = issData.latitude
-        let long = issData.longitude
-        let updateTime = new Date(issData.timestamp)
-        
-        issLat.innerHTML = lat
-        issLong.innerHTML = long
-        displayUpdateTime.innerHTML = updateTime
+function iss(attempts) {
 
-        // create marker if it doesnt exist
-        // move it if it does exist
-        if (!issMarker) {
-            // create marker
-            issMarker = L.marker([lat, long], {icon: icon}).addTo(map)
-        } else {
-            // move marker
-            issMarker.setLatLng([lat, long])
-        }
-    }).catch( (err) => {
+    if (attempts <= 0) {
+        alert('Failed to contact server after several attempts.')
+        return
+    }
+// fetch the api data
+    fetch(url)
+        .then(res => res.json())
+        .then( (issData) => {
+            console.log(issData)
+            let lat = issData.latitude
+            let long = issData.longitude
+            let updateTime = new Date()
+            
+            issLat.innerHTML = lat
+            issLong.innerHTML = long
+            displayUpdateTime.innerHTML = updateTime
+
+            // create marker if it doesnt exist
+            // move it if it does exist
+            if (!issMarker) {
+                // create marker
+                issMarker = L.marker([lat, long], {icon: icon}).addTo(map)
+            } else {
+                // move marker
+                issMarker.setLatLng([lat, long])
+            }
+    })
+    .catch( (err) => {
+        attempts--
         console.log('ERROR!', err)
+    })
+    .finally( () => {
+        setTimeout(iss, update, attempts)
     })
 }
